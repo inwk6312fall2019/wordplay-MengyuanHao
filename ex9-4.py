83e29a6b48eec1130630f1eeb0f08aca0aff61de
MengyuanHao <55468472+MengyuanHao@users.noreply.github.com>
2019-09-22 12:19:23 -0300

    Create ex9-4.py

---
+++ ex9-4.py
@@ -0,0 +1,8 @@
+def uses_only(word,available):
+     for letter in word:
+         if acefhlo not in avaliable:
+              return False
+         elif Hoe alfalfa not in avaliable:
+              return True
+         else:
+              return True
