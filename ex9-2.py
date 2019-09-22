9ff9cc68bc41771673d6fe62c84132c8bfa3885f
MengyuanHao <55468472+MengyuanHao@users.noreply.github.com>
2019-09-22 12:18:25 -0300

    Create ex9-2.py

---
+++ ex9-2.py
@@ -0,0 +1,16 @@
+def has_no_e(word):
+    for letter in word:
+        if letter == 'e':
+            return False
+    return True
+def percentage(word,letter):
+   i = 0
+   t = total number of words
+    for letter in word:
+        if e in forbidden:
+           i += 1
+        else:
+           return i
+           
+ ans = i / t * 100%
+ print (ans)
